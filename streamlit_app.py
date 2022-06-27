5d528a88942a4f6bddd1e9dc81eeb9606f2fc897
leopold guyot <leopold.guyot13@gmail.com>
2022-06-27 12:43:24 +0200

    First commit

---
+++ streamlit_app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+import librosa
+import librosa.display
+
+def graph_spectrogram(wav_file):
+    y, sr = librosa.load(wav_file)
+    D = librosa.stft(y)
+    S_db = librosa.amplitude_to_db(np.abs(D), ref=np.max)
+    fig, ax = plt.subplots()
+    img = librosa.display.specshow(S_db, ax=ax)
+    fig.colorbar(img, ax=ax)
+    
+    return plt.gcf()
+
+
+state = False
+st.markdown('''
+    ## A simple sonograph maker made in python
+    using the streamlit API
+    ''')
+
+uploaded_file = st.file_uploader("choose a .wav file", type = ".wav")
+example = st.checkbox("use an example file (willow tit)")
+
+if example : 
+    st.audio("willow_tit.wav", format = 'audio/wav')
+    st.pyplot(graph_spectrogram("willow_tit.wav"))    
+
+elif uploaded_file :
+    st.audio(uploaded_file, format = 'audio/wav')
+    st.pyplot(graph_spectrogram(uploaded_file)) 
+    
+#todo a download button for image output
+#todo allow interaction with figure ?
+#todo comparaison between spectrograms
+#todo allow more example (maybe one for each bird call in belgium) -> only interesting parts selectionned
+
+
+
+
+
